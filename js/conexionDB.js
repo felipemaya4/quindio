4cd2bc680764e5be1a367555460d08a3a171d27f
felipemaya4 <pipe.040691@gmail.com>
2024-04-26 10:58:05 -0500

    funciones para acceder a la DB

--- js/conexionDB.js
+++ js/conexionDB.js
@@ -1,11 +1,13 @@
-const supabaseURL = 'https://gwhwzptfplkizqegaljb.supabase.co';
+const supabaseURL = 'https://gwhwzptfplkizqegaljb.supabase.co';//url de la base de datos
+// api key  de la base de datos
 const supabaseAnonPublicKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3aHd6cHRmcGxraXpxZWdhbGpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE0OTEwOTQsImV4cCI6MjAyNzA2NzA5NH0.bxw-KcIoKKkds4d0guJucWDzpsIztd2FSARwiXy-A1M'
+//inicializando la conexion con el servidor
 const ClienteSupabase = supabase.createClient(supabaseURL,supabaseAnonPublicKey);
 const miHeaders = {
     "apikey": supabaseAnonPublicKey,
     "Authorization": 'Bearer '+supabaseAnonPublicKey
 }
-
+// funcion para insertar un estudio previo
 async function insertEstudioPrevio(dataDocument) {
     
     const {data, error} = await ClienteSupabase
@@ -15,6 +17,16 @@ async function insertEstudioPrevio(dataDocument) {
 
    console.log(error);
    console.log(data);
+}
+//funcion para obtener un estudio previo de la tabla
+async function getEstudioPrevio(numeroPrecontractual) {
+    const { data, error } = await ClienteSupabase
+    .from('estudios_Previos')
+    .select()
+    .eq('numero_estudio', numeroPrecontractual)
+    
+    console.log(error);
+    console.log(data);
 }
 
 async function getEstudioList() {
@@ -35,7 +47,14 @@ async function getEstudioList() {
     
 }
 
-async function getContratistas () {
+//funcion para obtener todos los contratistas de la tabla
+async function getContratistasList() {
     
+    const { data, error } = await ClienteSupabase
+    .from('contratista')
+    .select('*')
+    
+    console.log(error);
+    console.log(data);
 }
 
